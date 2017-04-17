48ccae3c96ec2c1ae7fb3b21fd4ec0554a04a894
ilsken <ilskenyf@gmail.com>
2017-04-17 10:25:03 +0800

    Add mobile

---
+++ app/Http/Controllers/MobileController.php
@@ -0,0 +1,38 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+class MobileController extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+    public function checkIn()
+    {
+
+
+    }
+
+    public function getRosterList()
+    {
+
+
+    }
+
+    public function getReport()
+    {
+
+
+    }
+
+    public function updatePassowrd()
+    {
+        $user = Auth::user();
+
+
+    }
+
+}
