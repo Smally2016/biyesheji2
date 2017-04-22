6e25e4254505f5809883c1f9c10cc93bec5bab0c
ilsken <ilskenyf@gmail.com>
2017-04-22 22:23:06 +0800

    Improve wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,9 +2,10 @@
 
 namespace Someline\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Log;
 
-class WechatController extends BaseController
+class WechatController extends Controller
 {
     /**
      * @return mixed
@@ -14,7 +15,7 @@ class WechatController extends BaseController
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function($message){
+        $wechat->server->setMessageHandler(function ($message) {
             return "欢迎关注 帅哥，请留下您的闺蜜联系方式！";
         });
 
