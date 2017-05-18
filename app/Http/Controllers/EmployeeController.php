d43ac0889f3e2c34364df145f1b37554b71f01c1
ilsken <ilskenyf@gmail.com>
2017-05-18 16:58:17 +0800

    Add baidu map

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -62,10 +62,11 @@ class EmployeeController extends Controller
         $data['name'] = strtoupper($data['name']);
         $employee = new EmployeeModel();
         if ($employee->create($data)) {
+            $employee = EmployeeModel::where('nric', $data['nric'])->first();
             $department_employee = new DepartmentEmployeeModel();
             $department_employee->create([
                 'department_id' => $data['department_id'],
-                'employee_id' => EmployeeModel::where('nric', $data['nric'])->first()->employee_id
+                'employee_id' => $employee->employee_id
             ]);
             Session::flash('success', 'Created Successfully');
             $phone_number = $data['phone'];
