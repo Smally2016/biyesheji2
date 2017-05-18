810e0bd1af5096237173d66af96b0f33997ab268
ilsken <ilskenyf@gmail.com>
2017-05-19 01:20:21 +0800

    Add wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -18,9 +18,8 @@ class WechatController extends Controller
         $oauth = $app->oauth;
 // 未登录
 
-        $wechat_user = $_SESSION['wechat_user'];
-        if (empty($_SESSION['wechat_user'])) {
-            $_SESSION['target_url'] = '/';
+        $wechat_user = session('wechat_user');
+        if (empty($wechat_user)) {
             return $oauth->redirect();
             // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
             // $oauth->redirect()->send();
