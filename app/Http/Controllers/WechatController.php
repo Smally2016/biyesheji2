9c97e10b649dc3380ad7aa63a83de7433186b9d5
ilsken <ilskenyf@gmail.com>
2017-05-19 01:24:23 +0800

    Add wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -13,18 +13,14 @@ class WechatController extends Controller
      */
     public function serve()
     {
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $app = new Application(config('wechat'));
-        $oauth = $app->oauth;
-// 未登录
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
+            return "欢迎关注 帅哥，请留下您的闺蜜联系方式！";
+        });
 
-        $wechat_user = session('wechat_user');
-        if (empty($wechat_user)) {
-            return $oauth->redirect();
-            // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
-            // $oauth->redirect()->send();
-        }
-
-        return redirect('/');
+        Log::info('return response.');
+        return $wechat->server->serve();
     }
 }
